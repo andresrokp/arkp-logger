ae736ba20e0ae317ecb3df4b4215f361de5af42f
andresrokp <afrokap@gmail.com>
2024-01-29 12:06:38 -0500

    feat: server up and working

---
+++ index-arkp-logger.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const log = console.log;
+
+const app = express();
+
+// To handle json in requests
+app.use(express.json());
+// To poner la verga console log
+app.use(morgan('[:date] :remote-addr :method :url :status :res[content-length] - :response-time ms'));
+
+// Middleware for pretty-printing JSON responses
+app.set('json spaces', 2);
+
+// Endpoint
+app.post('/', async (req, res) => {
+  // Example JSON response
+  log('...Logging message...');
+  log(new Date().toISOString());
+  log(req.body);
+
+  res.sendStatus(200);
+});
+
+// Server setup
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`arkp-logger server is listening to port ${PORT}`);
+});
